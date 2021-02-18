abd325b5989d93db57ddf3207ade00b9960d333e
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-02-17 21:02:01 -0300

    feat: add DbContext class

---
+++ VideoMonitoring.Infra.Data/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoMonitoring.Domain.Entities;
+
+namespace VideoMonitoring.Infra.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Server> Servers { get; set; }
+        public DbSet<Video> Videos { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
